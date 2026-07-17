ff8931bcc1a29544f4e3fd569eceee43117bfbeb
swapnil <130269745+swapnil7620@users.noreply.github.com>
2026-07-17 12:59:54 +0530

    discovery server to register other services

---
+++ discovery-server/src/test/java/com/swapnil/discovery_server/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swapnil.discovery_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
